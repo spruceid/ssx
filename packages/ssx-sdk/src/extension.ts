e638be22665bd18c0b948863e9f4a9e93d0293c4
Jacob <jacob.ward@spruceid.com>
2022-10-28 17:41:33 +0100

    Rename references to recap (#4)

--- packages/ssx-sdk/src/extension.ts
+++ packages/ssx-sdk/src/extension.ts
@@ -4,13 +4,13 @@ import { SSXSession, SiweConfig } from './types';
 
 /** Interface for an extension to SSX. */
 export interface SSXExtension {
-    /** [capgrok] Capability namespace. */
+    /** [recap] Capability namespace. */
     namespace?: string,
-    /** [capgrok] Default delegated actions in capability namespace. */
+    /** [recap] Default delegated actions in capability namespace. */
     defaultActions?(): Promise<string[]>,
-    /** [capgrok] Delegated actions by target in capability namespace. */
+    /** [recap] Delegated actions by target in capability namespace. */
     targetedActions?(): Promise<{ [target: string]: string[] }>,
-    /** [capgrok] Extra metadata to help validate the capability. */
+    /** [recap] Extra metadata to help validate the capability. */
     extraFields?(): Promise<ExtraFields>,
     /** Hook to run after SSX has connected to the user's wallet.
      * This can return an object literal to override the session configuration before the user
