0ebe2df8fbeb34033c6b9a073886d5528565031e
Samuel Gbafa <skgbafa@gmail.com>
2022-11-10 17:11:01 -0100

    script cleanup

--- scripts/pre-publish.js
+++ scripts/pre-publish.js
@@ -1,8 +1,4 @@
-const { readJson, readdir, pathExists, writeJSON } = require('fs-extra');
-
-
-const packageJSON = "package.json" 
-
+const { readJSON, readdir, pathExists, writeJSON } = require('fs-extra');
 
 const writePackages = async (packages) => {
     for (const package in packages) {
@@ -15,23 +11,23 @@ const getAllPackageJSON = async () => {
     // read package.json
     const packageList = []
     const packages = {};
-    const { workspaces } = await readJson(packageJSON);
+    const { workspaces } = await readJSON("package.json");
 
     // get packages in workspaces
     for (const workspace of workspaces){
         if (workspace.includes("/*")) {
             const currentWorkspace = workspace.replace("*", "");
             const dirs = await readdir(currentWorkspace)
-            packageList.push(...dirs.map((dir) => `${currentWorkspace}${dir}/${packageJSON}`))
+            packageList.push(...dirs.map((dir) => `${currentWorkspace}${dir}/package.json`))
         } else {
-            packageList.push(`${workspace}/${packageJSON}`)
+            packageList.push(`${workspace}/package.json`)
         }
     }
     
     // get package json
     for (const package of packageList) {
         if (await pathExists(package)) {
-            const data = await readJson(package)
+            const data = await readJSON(package)
             packages[data.name] = {
                 location: package,
                 version: data.version,
@@ -40,45 +36,27 @@ const getAllPackageJSON = async () => {
         }
     }
 
-    // writePackages(packages)
-    // console.log(packages)
     return packages;
 }
 
 const updatePackageVersion = (packages, usePublishedVersion = true) => {
     console.log("Using local package versions");
     const packageNames = Object.keys(packages);
-    // console.log(packages);
-    console.log(packageNames);
     for (const package in packages) {
-        // console.log(package)
         // in each package, check for package names
         for (const dependency in packages[package].data.dependencies) {
-            // console.log(packageNames)
             if (packageNames.includes(dependency)) {
-                let newVersion = "*";
-                if (usePublishedVersion) {
-                    newVersion = packages[dependency].version;
-                }
-                console.log(dependency)
+                const newVersion = usePublishedVersion ? packages[dependency].version : "*";
                 packages[package].data.dependencies[dependency] = newVersion;
-                // if found, set to *
-                // or set to version
             }
         }
     }
     return packages;
-    
-}
-
-const usePublicPackages = (packages) => {
-    console.log("Using public package versions");
 }
 
 const main = async () => {
     const usePublishedVersion = !process.argv.includes("local");
     const packages = await getAllPackageJSON();
-    // console.log(packages)
     const updatedPackages = updatePackageVersion(packages, usePublishedVersion);
     writePackages(updatedPackages);
 }
