d7af48acb90060b5c42ceb1e6ed7766424af6e58
Gregorio <greg.magalhaes@gmail.com>
2022-11-28 10:50:21 -0300

    Testing broken test

--- packages/ssx-gnosis-extension/tests/server.test.ts
+++ packages/ssx-gnosis-extension/tests/server.test.ts
@@ -1,8 +1,11 @@
 import { ethers, Wallet } from "ethers";
-import { SiweMessage } from "siwe";
+import { SiweError, SiweErrorType, SiweMessage } from "siwe";
 import { SiweGnosisVerify, GnosisDelegation } from "../src";
 
-const provider = new ethers.providers.InfuraProvider("goerli", "49a0efa3aaee4fd99797bfa94d8ce2f1");
+const provider = new ethers.providers.InfuraProvider(
+  "goerli",
+  "49a0efa3aaee4fd99797bfa94d8ce2f1"
+);
 const wallet = Wallet.createRandom();
 
 test("Instantiate GnosisDelegation successfully", () => {
@@ -13,23 +16,31 @@ test("Instantiate GnosisDelegation successfully", () => {
 
 test("Should not verify message when there is no delegation", async () => {
   const message = new SiweMessage({
-    "domain": "ssx.id",
-    "address": "0x9D85ca56217D2bb651b00f15e694EB7E713637D4",
-    "statement": "Sign-In With Ethereum Example Statement",
-    "uri": "https://ssx.id",
-    "version": "1",
-    "nonce": "bTyXgcQxn2htgkjJn",
-    "chainId": 1,
-    "expirationTime": "2100-02-28T14:31:43.952Z"
+    domain: "ssx.id",
+    address: "0x9D85ca56217D2bb651b00f15e694EB7E713637D4",
+    statement: "Sign-In With Ethereum Example Statement",
+    uri: "https://ssx.id",
+    version: "1",
+    nonce: "bTyXgcQxn2htgkjJn",
+    chainId: 1,
+    expirationTime: "2100-02-28T14:31:43.952Z",
   });
   const signature = await wallet.signMessage(message.prepareMessage());
-  expect(message.verify(
-    { signature },
-    {
-      verificationFallback: SiweGnosisVerify,
-      provider
-    }
-  )).resolves.not.toThrowError();
+  await message
+    .verify(
+      { signature },
+      {
+        verificationFallback: SiweGnosisVerify,
+        provider,
+      }
+    )
+    .catch(({ success, error }) => {
+      expect(success).toBeFalsy();
+      expect(error).not.toBeNull();
+      expect(error.type).toBe(
+        "Given address is not registered at delegee list."
+      );
+    });
 });
 
 // TODO: test case when delegate is successfull
