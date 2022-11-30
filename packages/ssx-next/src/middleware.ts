bc8dc02382f28de12308e06d6024e7eaf0a99482
Samuel Gbafa <skgbafa@gmail.com>
2022-11-30 12:49:34 -0800

    middleware outline

--- packages/ssx-next/src/middleware.ts
+++ packages/ssx-next/src/middleware.ts
@@ -1,1 +1,29 @@
-export const log = console.log;
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import type { SSXServer } from '@spruceid/ssx-server';
+
+export function ssxMiddleware(request: NextRequest, ssx: SSXServer) {
+  if (request.nextUrl.pathname.includes('/ssx-nonce')) {
+    console.log('[Middleware] ssx-nonce');
+    // request.session.nonce = ssx.generateNonce();
+
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.includes('/ssx-login')) {
+    console.log('[Middleware] ssx-login');
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.includes('/ssx-logout')) {
+    console.log('[Middleware] ssx-logout');
+    return NextResponse.next();
+  }
+
+  if (request.nextUrl.pathname.includes('/ssx-me')) {
+    console.log('[Middleware] ssx-me');
+    return NextResponse.next();
+  }
+
+  return NextResponse.next();
+}
