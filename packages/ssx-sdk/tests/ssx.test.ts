8e57269e5055f5204e8c39e2d40f76e8f9d993e1
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2022-12-02 15:50:56 -0300

    Setup jest and add some tests (#27)
    * Remove duplicated dependency
    
    * Bump version
    
    * Jest initial config
    
    * Jest config
    
    * Add base tests for all packages
    
    * Test GH action and coverage badge
    
    * Remove test from builld
    
    * Fix coverage command
    
    * Get coverage from sed
    
    * Add tests
    
    * Testing broken test
    
    * Move test command out of echo
    
    * Fix test and update build action
    
    * Move to codecov
    
    * Update CI
    
    * Remove badge from template
    
    * Remove extra action and consolidate under build
    
    * Update coverage badge
    
    Co-authored-by: Juliano <julianocezargyn@hotmail.com>
    Co-authored-by: Sam Gbafa <skgbafa@gmail.com>

--- packages/ssx-sdk/tests/ssx.test.ts
+++ packages/ssx-sdk/tests/ssx.test.ts
@@ -1,29 +1,29 @@
-const { generateTestingUtils } = require('eth-testing');
-const { TextEncoder, TextDecoder } = require('util');
+const { generateTestingUtils } = require("eth-testing");
+const { TextEncoder: TE, TextDecoder: TD } = require("util");
 
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+global.TextEncoder = TE;
+global.TextDecoder = TD;
 
-const { SSX } = require('../src');
+const { SSX } = require("../src");
 
-const testingUtils = generateTestingUtils({ providerType: 'MetaMask' });
+const testingUtils = generateTestingUtils({ providerType: "MetaMask" });
 
 beforeAll(() => {
   // Manually inject the mocked provider in the window as MetaMask does
-  global.window.ethereum = testingUtils.getProvider();
+  (global.window as any).ethereum = testingUtils.getProvider();
 });
 
 afterEach(() => {
   testingUtils.clearAllMocks();
 });
 
-test('Instantiate SSX with window.ethereum', () => {
+test("Instantiate SSX with window.ethereum", () => {
   expect(() => {
     const ssx = new SSX();
   }).not.toThrowError();
 });
 
-test('Instantiate SSX with providers.web3.driver', () => {
+test("Instantiate SSX with providers.web3.driver", () => {
   expect(() => {
     const config = {
       providers: {
@@ -42,17 +42,17 @@ test('Instantiate SSX with providers.web3.driver', () => {
 
 // test('Sign-in with Ethereum', () => {
 //   // TODO: sign request with mock provider
-//   // expect(async () => {
-//   //   const config = {
-//   //     providers: {
-//   //       web3: {
-//   //         driver: testingUtils.getProvider(),
-//   //       },
-//   //     },
-//   //   };
-//   //   const ssx = new SSX(config);
-//   //   await ssx.signIn();
-//   // }).not.toThrowError();
+//   expect(async () => {
+//     const config = {
+//       providers: {
+//         web3: {
+//           driver: provider,
+//         },
+//       },
+//     };
+//     const ssx = new SSX(config);
+//     await ssx.signIn();
+//   }).not.toThrowError();
 // });
 
 // test('Throw Error if Ethereum Wallet isn\'t found', () => {
