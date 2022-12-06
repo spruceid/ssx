29ab2d989cb318ec69e521e386d7d38fb52f7f3a
Samuel Gbafa <skgbafa@gmail.com>
2022-12-06 13:39:29 -0500

    add additional provider options

--- packages/ssx-react/src/ssx.tsx
+++ packages/ssx-react/src/ssx.tsx
@@ -2,12 +2,22 @@ import { useContext, createContext, useState, useEffect, ReactNode } from 'react
 import { SSX, SSXClientConfig } from "@spruceid/ssx";
 import { useSigner } from 'wagmi';
 
+/** Interface for SSX Web3 Provider. */
+export interface SSXWeb3Provider {
+  /** web3 Provider. */
+  provider: any;
+  /** web3 Provider Loaded. */
+  providerLoaded: boolean;
+}
+
 /** Props for SSX Provider. */
 export interface SSXProviderProps {
   /** Optional SSX configuration, used for instantiating an SSX Instance. */
   ssxConfig?: SSXClientConfig;
   /** Provider child nodes, for rendering. */
   children: ReactNode;
+  /** Optional SSX Signer. Will . */
+  web3Provider?: SSXWeb3Provider;
 }
 
 /** Interface for contents provided to the Hook. */
@@ -27,8 +37,24 @@ const defaultContext: SSXContextInterface = {
 const SSXContext = createContext(defaultContext);
 
 /** SSX Provider Component. */
-export const SSXProvider = ({ ssxConfig, children }: SSXProviderProps) => {
-  const { data: signer, isSuccess: signerLoaded } = (typeof window !== 'undefined' && useSigner()) || { data: undefined, isSuccess: false };
+export const SSXProvider = ({
+  ssxConfig,
+  children,
+  web3Provider,
+}: SSXProviderProps) => {
+  let provider, providerLoaded;
+  let usingWagmi = false;
+  if (web3Provider) {
+    provider = web3Provider.provider;
+    providerLoaded = web3Provider.providerLoaded || true;
+  } else {
+    // assume using wagmi.sh if no provider is provided
+    usingWagmi = true;
+    ({ data: provider, isSuccess: providerLoaded } = (typeof window !==
+      'undefined' &&
+      useSigner()) || { data: undefined, isSuccess: false });
+  }
+
   const [ssx, setSSX] = useState<SSX>();
   const [ssxLoaded, setSSXLoaded] = useState(false);
 
@@ -40,7 +66,7 @@ export const SSXProvider = ({ ssxConfig, children }: SSXProviderProps) => {
         providers: {
           ...ssxConfig?.providers,
           web3: {
-            driver: signer?.provider,
+            driver: usingWagmi ? provider?.provider : provider,
             ...ssxConfig?.providers?.web3,
           },
         }
@@ -49,10 +75,10 @@ export const SSXProvider = ({ ssxConfig, children }: SSXProviderProps) => {
       setSSX(ssxInstance);
       setSSXLoaded(true);
     }
-    if (signerLoaded && signer) {
+    if (providerLoaded && provider) {
       initializeSSX();
     }
-  }, [signer, signerLoaded, ssxConfig]);
+  }, [provider, providerLoaded, ssxConfig]);
 
   const SSXProviderValue: SSXContextInterface = {
     ssx,
