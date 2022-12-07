51194afdeab1ad31bda28b3e30fe8eab5f49bb2f
Samuel Gbafa <skgbafa@gmail.com>
2022-12-06 19:25:21 -0500

    lint

--- packages/ssx-sdk/src/index.ts
+++ packages/ssx-sdk/src/index.ts
@@ -3,11 +3,11 @@ export * from './core';
 export * from '@spruceid/ssx-core/dist/client/types';
 export * from '@spruceid/ssx-core/dist/types';
 export {
-    /** @deprecated use SSXClientConfig field instead */
-    SSXClientConfig as SSXConfig,
-    /** @deprecated use SSXClientProviders field instead */
-    SSXClientProviders as SSXProviders,
-    /** @deprecated use SSXClientSession field instead */
-    SSXClientSession as SSXSession,
+  /** @deprecated use SSXClientConfig field instead */
+  SSXClientConfig as SSXConfig,
+  /** @deprecated use SSXClientProviders field instead */
+  SSXClientProviders as SSXProviders,
+  /** @deprecated use SSXClientSession field instead */
+  SSXClientSession as SSXSession,
 } from '@spruceid/ssx-core';
 export { SiweMessage } from 'siwe';
