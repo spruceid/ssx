0c8e9788082c293a199abf3803c6b449149205be
Samuel Gbafa <skgbafa@gmail.com>
2022-12-13 00:09:12 -0500

    add credential config

--- packages/ssx-next/src/nextAuth.ts
+++ packages/ssx-next/src/nextAuth.ts
@@ -1,41 +1,40 @@
-import CredentialsProvider from "next-auth/providers/credentials";
+import type { CredentialsConfig } from 'next-auth/providers/credentials';
+import { SiweMessage, SSXServer } from '@spruceid/ssx-server';
 
+const ssxCredentialProviderOptions: CredentialsConfig = {
+  name: 'Ethereum',
+  credentials: {
+    message: {
+      label: 'Message',
+      type: 'text',
+      placeholder: '0x0',
+    },
+    signature: {
+      label: 'Signature',
+      type: 'text',
+      placeholder: '0x0',
+    },
+  },
+  async authorize(credentials) {
+    try {
+      const siwe = new SiweMessage(JSON.parse(credentials?.message || '{}'));
+      const nextAuthUrl = new URL(process.env.DOMAIN as string);
+      if (siwe.domain !== nextAuthUrl.host) {
+        return null;
+      }
 
-const ssxCredentialProviderOptions = {
-        name: "Ethereum",
-        credentials: {
-          message: {
-            label: "Message",
-            type: "text",
-            placeholder: "0x0",
-          },
-          signature: {
-            label: "Signature",
-            type: "text",
-            placeholder: "0x0",
-          },
-        },
-        async authorize(credentials) {
-            try {
-              console.log("hoge");
-              const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"));
-              const nextAuthUrl = new URL(process.env.DOMAIN as string);
-              if (siwe.domain !== nextAuthUrl.host) {
-                return null;
-              }
-    
-              if (siwe.nonce !== (await getCsrfToken({ req }))) {
-                return null;
-              }
-    
-              await siwe.validate(credentials?.signature || "");
-              return {
-                id: siwe.address,
-              };
-            } catch (e) {
-              return null;
-            }
-          },
-}
+      if (siwe.nonce !== (await getCsrfToken({ req }))) {
+        return null;
+      }
 
-const SSXCredentialsProvider  =
+      await siwe.validate(credentials?.signature || '');
+      return {
+        id: siwe.address,
+      };
+    } catch (e) {
+      return null;
+    }
+  },
+};
+
+// export ssxCredentialProviderOptions, authorize function, session function,
