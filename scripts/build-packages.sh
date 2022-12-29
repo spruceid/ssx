2fbad590d006f9fa44fe5460d62fb8d72e1edc18
Samuel Gbafa <skgbafa@gmail.com>
2022-12-29 15:39:37 -0800

    fix builds

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 echo "[SSX Build Packages] Prebuild ssx-sdk for ssx-gnosis-extension"
+yarn core build
 yarn sdk build
 yarn gnosis build
+yarn middleware build
+yarn react build
 echo "[SSX Build Packages] Building all SSX Packages"
 npx lerna run build
 npx lerna bootstrap
