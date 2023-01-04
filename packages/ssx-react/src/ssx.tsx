d1392995780d3c1d1bd3aa3acd427bad08cc1288
Samuel Gbafa <skgbafa@gmail.com>
2023-01-03 17:48:20 -0800

    remove other approach

--- packages/ssx-react/src/ssx.tsx
+++ packages/ssx-react/src/ssx.tsx
@@ -8,14 +8,6 @@ import {
 } from 'react';
 import { SSX, SSXClientConfig } from '@spruceid/ssx';
 import { useSigner } from 'wagmi';
-// /** Approach B */
-// import { getCsrfToken, signIn, signOut } from 'next-auth/react';
-// import type {
-//   SignInOptions,
-//   SignOutParams,
-//   SignInAuthorizationParams,
-// } from 'next-auth/react';
-// /** Approach B */
 
 /** Interface for SSX Web3 Provider. */
 export interface SSXWeb3Provider {
@@ -41,25 +33,12 @@ export interface SSXContextInterface {
   ssx: SSX | undefined;
   /** SSX Instance loading state. */
   ssxLoaded: boolean;
-  // /** Approach B */
-  // /** NextAuth Sign In function. */
-  // signIn?: (
-  //   options?: SignInOptions,
-  //   authorizationParams?: SignInAuthorizationParams
-  // ) => Promise<any>;
-  // /** NextAuth Sign Out function. */
-  // signOut?: (options?: SignOutParams) => Promise<any>;
-  // /** Approach B */
 }
 
 /** Default, uninitialized context. */
 const defaultContext: SSXContextInterface = {
   ssx: undefined,
   ssxLoaded: false,
-  // /** Approach B */
-  // signIn: undefined,
-  // signOut: undefined,
-  // /** Approach B */
 };
 
 const SSXContext = createContext(defaultContext);
@@ -95,82 +74,14 @@ export const SSXProvider = ({
   const setSSX = (ssx: SSX) => setSSXState({ ssx });
   const setSSXLoaded = (ssxLoaded: boolean) => setSSXState({ ssxLoaded });
 
-  // /** Approach B */
-  // const nextSignIn = async (
-  //   options?: SignInOptions,
-  //   authorizationParams?: SignInAuthorizationParams
-  // ) => {
-  //   if (ssxLoaded) {
-  //     const csrfToken = await getCsrfToken();
-  //     // inject nonce into siweConfig
-  //     const modifiedSSXConfig = {
-  //       ...ssxConfig,
-  //       siweConfig: {
-  //         ...ssxConfig?.siweConfig,
-  //         nonce: csrfToken,
-  //       },
-  //     };
-  //     const ssxInstance = new SSX(modifiedSSXConfig);
-  //     const { siwe, signature } = await ssxInstance.signIn();
-  //     return signIn(
-  //       'credentials',
-  //       { message: siwe, signature, ...options },
-  //       authorizationParams
-  //     );
-  //   }
-  // };
-  // const nextSignOut = async (options?: SignOutParams) => {
-  //   if (ssxLoaded) {
-  //     const csrfToken = await getCsrfToken();
-  //     // inject nonce into siweConfig
-  //     const modifiedSSXConfig = {
-  //       ...ssxConfig,
-  //       siweConfig: {
-  //         ...ssxConfig?.siweConfig,
-  //         nonce: csrfToken,
-  //       },
-  //     };
-  //     const ssxInstance = new SSX(modifiedSSXConfig);
-  //     await ssxInstance.signOut();
-  //     return signOut(options);
-  //   }
-  // };
-
-  // /** Approach B */
-
-  // const nextSignIn = async (
-  //   options?: SignInOptions,
-  //   authorizationParams?: SignInAuthorizationParams
-  // ) => {
-  //   if (ssxLoaded) {
-  //     // const csrfToken = await getCsrfToken();
-  //     // inject nonce into siweConfig
-  //     const { siwe, signature } = await ssx?.signIn();
-  //     return signIn(
-  //       'credentials',
-  //       { message: siwe, signature, ...options },
-  //       authorizationParams
-  //     );
-  //   }
-  // };
-  // const nextSignOut = async (options?: SignOutParams) => {
-  //   if (ssxLoaded) {
-  //     await ssx?.signOut();
-  //     return signOut(options);
-  //   }
-  // };
-  // /** Approach B */
-
   useEffect(() => {
     async function initializeSSX() {
       const { SSX } = await import('@spruceid/ssx');
-      // const csrfToken = await getCsrfToken(); // Approach B
 
       const modifiedSSXConfig = {
         ...ssxConfig,
         siweConfig: {
           ...ssxConfig?.siweConfig,
-          // nonce: csrfToken,  // Approach B
         },
         providers: {
           ...ssxConfig?.providers,
@@ -192,10 +103,6 @@ export const SSXProvider = ({
   const SSXProviderValue: SSXContextInterface = {
     ssx,
     ssxLoaded,
-    // /** Approach B */
-    // signIn: nextSignIn,
-    // signOut: nextSignOut,
-    // /** Approach B */
   };
 
   return (
