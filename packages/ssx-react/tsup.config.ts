91e5260222622e049e097792a40666735b5ea85a
Samuel Gbafa <skgbafa@gmail.com>
2023-01-09 19:20:42 -0800

    Use tsup for builds

---
+++ packages/ssx-react/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/*'],
+  clean: true,
+  format: ['esm', 'cjs'],
+  legacyOutput: true,
+  dts: {
+    compilerOptions: {
+      jsx: 'react-jsx',
+    },
+  },
+});
