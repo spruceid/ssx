ca450097466045f64c8223f547b034e310bb400d
Juliano <julianocezargyn@hotmail.com>
2023-01-12 13:51:25 -0300

    Update standalone example title

--- examples/ssx-test-web3modal-v2-standalone/src/components/Title.js
+++ examples/ssx-test-web3modal-v2-standalone/src/components/Title.js
@@ -1,7 +1,7 @@
 const Title = () => {
     return <div className='Title'>
         <h1 className='Title-h1'>
-            SSX Test <code>@web3modal/react</code>
+            SSX Test <code>@web3modal/standalone</code>
         </h1>
         <h2 className='Title-h2'>
             Connect and sign in with your Ethereum account
