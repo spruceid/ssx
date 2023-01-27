b6978a5a79fae853b881e9965edd461846492964
franklovefrank <acfields11@gmail.com>
2023-01-27 11:39:38 -0500

    compiled artifacts

--- examples/ssx-test-social/hardhat.config.ts
+++ examples/ssx-test-social/hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
 
-export default config;
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.0",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
