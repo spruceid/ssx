bcb6a45e1e44b24f0751fac5ea5f611adda2962b
Juliano <julianocezargyn@hotmail.com>
2023-02-21 10:12:38 -0300

    Add nextauth email example to the workspace and remove ssx-react/next-auth references

--- examples/ssx-test-nextauth/pages/api/auth/[...nextauth].ts
+++ examples/ssx-test-nextauth/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { SSXNextAuth } from "@spruceid/ssx-react/next-auth/backend";
+import { SSXNextAuth } from "@spruceid/ssx-authjs/server";
 import { SSXServer } from "@spruceid/ssx-server";
 
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
@@ -23,7 +23,7 @@ export default async function auth(req: NextApiRequest, res: NextApiResponse) {
     },
     secret: process.env.NEXT_AUTH_SECRET,
     callbacks: {
-     session,
+      session,
     },
   });
 }
