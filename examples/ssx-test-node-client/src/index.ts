39dbe3a1ec026a57621732c4e8b21541d8fea32e
Samuel Gbafa <skgbafa@gmail.com>
2023-03-07 23:11:27 -0800

    downgrade ethers version in example

--- examples/ssx-test-node-client/src/index.ts
+++ examples/ssx-test-node-client/src/index.ts
@@ -1,19 +1,21 @@
 import { SSX } from '@spruceid/ssx';
 import { ethers } from 'ethers';
+import { config } from 'dotenv';
+config();
 
-
+const testMessage = 'Example `personal_sign` message';
+const testSeed = process.env.SEED_PHRASE;
 
 const main = async () => {
-    const wallet = ethers.Wallet.createRandom();
+    const wallet = testSeed ? ethers.Wallet.fromMnemonic(testSeed) : ethers.Wallet.createRandom();
+    const driver = new ethers.providers.JsonRpcProvider("https://eth.rpc.blxrbdn.com");
+    const signer = wallet.connect(driver);
 
-    const signer = wallet.connect(
-        new ethers.JsonRpcProvider("https://eth.rpc.blxrbdn.com")
-    );
     const ssx = new SSX({
         resolveEns: false,
         enableDaoLogin: false,
         providers: {
-            web3: { driver: signer.provider },
+            web3: { driver: driver as ethers.providers.Web3Provider, signer },
         },
     });
     
