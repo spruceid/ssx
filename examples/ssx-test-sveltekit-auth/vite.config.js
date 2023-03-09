7689658d8ceb27af73127c92750fca8da0fc2741
franklovefrank <annaclaire.fields@spruceid.com>
2023-03-09 09:59:09 +0100

    define process.env

--- examples/ssx-test-sveltekit-auth/vite.config.js
+++ examples/ssx-test-sveltekit-auth/vite.config.js
@@ -3,6 +3,9 @@ import { sveltekit } from "@sveltejs/kit/vite"
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  define: {
+    'process.env': process.env
+  }
 }
 
 export default config
